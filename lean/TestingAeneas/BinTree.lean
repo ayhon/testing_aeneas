10ed04d25e14d07db1e12427f0944e348f6426b3
ayhon <ayhonnick@gmail.com>
2025-02-10 17:18:48 +0100

    feat: contains_insert and contains_iff_contains_reverse

--- lean/TestingAeneas/BinTree.lean
+++ lean/TestingAeneas/BinTree.lean
@@ -165,6 +165,131 @@ theorem rust_length_insert'(tree: BinTree α)(a: α)
       apply length_insert
     scalar_tac
 
+
+section Contains
+
+def Tree.contains[BEq α]: Tree α -> α -> Bool
+| .nil, _ => false
+| .node value lhs rhs, target => value == target || Tree.contains lhs target || Tree.contains rhs target
+
+abbrev is_proper_eq[BEq α](o: testing_aeneas.core.cmp.Eq α) : Prop
+:= ∀ (a b: α), o.PartialEqInst.eq a b = Result.ok (a == b)
+
+@[pspec]
+theorem contains_spec[BEq α](tree: BinTree α)(target: α)
+(proper_eq_instance: is_proper_eq o)
+: ∃ b,
+    BinTree.contains o tree target = Result.ok b ∧
+    Tree.contains (tree : Tree α) target = b
+:= by
+  rw [BinTree.contains]
+  match tree with
+  | .Nil => simp [toTree, Tree.contains]
+  | .Node v lhs rhs => 
+    simp [proper_eq_instance]
+    split
+    case isTrue found =>
+      simp [toTree, Tree.contains,
+            found]
+    case isFalse not_found => 
+      progress as ⟨is_in_left, is_in_left_spec⟩
+      split
+      case isTrue found_left => 
+        simp [toTree, Tree.contains,
+              is_in_left_spec, found_left]
+      case isFalse not_found_left =>
+        progress as ⟨is_in_right, is_in_right_spec⟩
+        simp [toTree, Tree.contains,
+              is_in_left_spec, not_found_left,
+              not_found,
+              is_in_right_spec]
+
+end Contains
+
+section Reverse
+def Tree.reverse: Tree α -> Tree α
+| .nil => .nil
+| .node value lhs rhs => .node value (Tree.reverse rhs) (Tree.reverse lhs)
+
+@[pspec]
+theorem reverse_spec(tree: BinTree α)
+: ∃ tree', 
+    BinTree.reverse tree = Result.ok tree' ∧
+    Tree.reverse (tree: Tree α) = tree'
+:= by
+  rw [BinTree.reverse]
+  match tree with
+  | .Nil => simp[Tree.reverse, toTree]
+  | .Node v lhs rhs => 
+    simp[Tree.reverse, toTree]
+    progress as ⟨lhs_rev, lhs_rev_spec⟩
+    progress as ⟨rhs_rev, rhs_rev_spec⟩
+    simp [toTree, lhs_rev_spec, rhs_rev_spec]
+end Reverse
+
+theorem contains_iff_reverse_contains[BEq α](tree: Tree α)(target: α)
+: Tree.contains tree target = (Tree.contains (Tree.reverse tree) target)
+:= by
+  match tree with
+  | .nil => simp [Tree.contains, Tree.reverse]
+  | .node v lhs rhs =>
+    generalize h: Tree.contains (Tree.node v lhs rhs) target = b
+    simp [Tree.contains, Tree.reverse] at *
+    match b with
+    | true => 
+      simp at *
+      obtain ⟨h | h⟩ := h
+      · simp[h]
+      · apply Or.inr
+        rw [<-contains_iff_reverse_contains]
+        assumption
+      · apply Or.inl ∘ Or.inr
+        rw [<-contains_iff_reverse_contains]
+        assumption
+    | false =>
+      simp at *
+      split_conjs <;> ((try rw [<-contains_iff_reverse_contains]); simp[h])
+
+attribute [-simp] Bool.exists_bool
+
+theorem rust_contains_iff_reverse_contains[BEq α](tree: BinTree α)(target: α)
+(proper_eq_instance: is_proper_eq o)
+: ∃ treeᵣ b1 b2,
+    BinTree.reverse tree = Result.ok treeᵣ ∧
+    BinTree.contains o tree target  = Result.ok b1 ∧
+    BinTree.contains o treeᵣ target = Result.ok b2 ∧
+    b1 = b2
+:= by
+  progress as ⟨treeᵣ, treeᵣ_spec⟩; simp
+  progress as ⟨b, b1_spec⟩
+  progress as ⟨b, b2_spec⟩
+  subst b b1_spec
+  rw [<-treeᵣ_spec]
+  symm
+  apply contains_iff_reverse_contains
+
+theorem contains_insert[BEq α][LawfulBEq α](tree: Tree α)(target: α)
+: Tree.contains (Tree.insert tree target) target = true
+:= by
+  match tree with
+  | .nil => 
+    simp [Tree.insert, Tree.contains]
+  | .node v lhs rhs => 
+    simp [Tree.insert, Tree.contains]
+    apply Or.inr
+    apply contains_insert
+
+@[pspec]
+theorem rust_contains_insert[BEq α][LawfulBEq α](tree: BinTree α)(target: α)
+(proper_eq_instance: is_proper_eq o)
+: ∃ treeᵢ,
+  tree.insert target = Result.ok treeᵢ ∧
+  treeᵢ.contains o target = Result.ok true
+:= by
+  progress as ⟨treeᵣ, treeᵣ_spec⟩
+  progress as ⟨b, b_spec⟩
+  rw [<-treeᵣ_spec] at b_spec
+  rw [contains_insert] at b_spec
+  exact b_spec.symm
+
 end Lemmas
-
-
