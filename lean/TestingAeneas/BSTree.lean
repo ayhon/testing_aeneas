008849215abe2003668f873b3f47f8f00d90430f
ayhon <ayhonnick@gmail.com>
2025-02-12 15:14:32 +0100

    feat: do a Rust-def → Lean-def → Set refinement

--- lean/TestingAeneas/BSTree.lean
+++ lean/TestingAeneas/BSTree.lean
@@ -3,9 +3,6 @@ import Aeneas
 
 open Aeneas.Std
 
-attribute [-simp] Bool.exists_bool
-
-section SetImpl/- {{{ -/
 
 namespace Spec/- {{{ -/
 open testing_aeneas
@@ -41,6 +38,37 @@ instance: Coe (BSTree α) (Set α) where
 end Translation/- }}} -/
 
 namespace Lemmas/- {{{ -/
+attribute [-simp] Bool.exists_bool
+attribute [local simp] Spec.contains
+attribute [local simp] Spec.insert
+open BSTree (toSpec)
+
+section TreeRefinement/- {{{ -/
+
+@[pspec]
+theorem tree_contains_spec(tree: BSTree Isize)(target: Isize)
+: ∃ b, 
+    BSTreeIsize.contains tree target = Result.ok b ∧
+    Spec.contains tree target = b
+:= by
+  cases tree <;> rw [BSTreeIsize.contains] <;> simp
+  case Node curr left right =>
+    split_ifs <;> (try progress with tree_contains_spec as ⟨in_left, in_left_spec⟩) <;> simp [*]
+
+@[pspec]
+theorem tree_insert_spec(tree: BSTree Isize)(value: Isize)
+: ∃ tree',
+    BSTreeIsize.insert tree value = Result.ok tree' ∧
+    Spec.insert value tree = tree'
+:= by
+  cases tree <;> rw [BSTreeIsize.insert] <;> simp
+  case Node curr left right =>
+    split_ifs <;> (try progress with tree_insert_spec as ⟨tree', tree'_spec⟩) <;> simp [*]
+
+end TreeRefinement/- }}} -/
+
+section SetRefinement/- {{{ -/
+
 @[simp] def well_formed[PartialOrder α][IsTotal α (·<=·)]: BSTree α -> Prop
 | .Nil => True
 | .Node curr left right =>
@@ -48,7 +76,7 @@ namespace Lemmas/- {{{ -/
     (∀ x ∈ (left: Set α),  x < curr) ∧
     (∀ x ∈ (right: Set α), curr < x)
 
-theorem left_disjoint_right_of_wf[PartialOrder α][IsTotal α (·<=·)]
+theorem left_right_disjoint_of_wf[PartialOrder α][IsTotal α (·<=·)]
   {curr: α}{left right: BSTree α}
 : well_formed (.Node curr left right) -> Disjoint (toSpec left) (toSpec right)
 := by
@@ -62,33 +90,30 @@ theorem left_disjoint_right_of_wf[PartialOrder α][IsTotal α (·<=·)]
   have := lt_irrefl x
   contradiction
 
-@[pspec]
-theorem contains_spec(tree: BSTree Isize)(target: Isize)
+theorem contains_spec{tree: BSTree Isize}(target: Isize)
 : well_formed tree 
--> ∃ b,
-    BSTreeIsize.contains tree target = Result.ok b ∧
-    (b = true ↔ target ∈ (tree : Set Isize))
+-> (Spec.contains tree target ↔ target ∈ (tree : Set Isize))
 := by 
   intro well_formed
-  rw [BSTreeIsize.contains]
   cases tree <;> simp at *
   case Node curr left right =>
-    have left_disjoint_right := left_disjoint_right_of_wf well_formed
+    have left_disjoint_right := left_right_disjoint_of_wf well_formed
     obtain ⟨left_wf, right_wf, left_inv, right_inv⟩ := well_formed
-    split_ifs <;> simp [*]
-    case pos curr_neq_target target_lt_curr =>
-      progress as ⟨left_contains_target, left_contains_target_spec⟩
-      if left_contains_target then
+    split_ifs <;> try simp [*]
+    case pos target_lt_curr =>
+      have := contains_spec target left_wf
+      /- progress as ⟨left_contains_target, left_contains_target_spec⟩ -/
+      if Spec.contains left target then
         simp_all
       else
         simp_all
         intro target_in_right
         have := right_inv _ target_in_right
         omega
-    case neg curr_neq_target target_ge_curr =>
+    case neg target_ge_curr =>
       have target_ge_curr := (le_of_not_gt target_ge_curr)
-      progress as ⟨right_contains_target, right_contains_target_spec⟩
-      if right_contains_target then
+      have := contains_spec target right_wf
+      if Spec.contains right target then
         simp_all
       else
         simp_all
@@ -96,40 +121,36 @@ theorem contains_spec(tree: BSTree Isize)(target: Isize)
         have := left_inv _ target_in_left
         omega
 
-@[pspec]
 theorem insert_spec(tree: BSTree Isize)(value: Isize)
 : well_formed tree
--> ∃ tree',
-    BSTreeIsize.insert tree value = Result.ok tree' ∧
-    insert value (toSpec tree) = ↑tree' ∧
-    well_formed tree'
+-> let tree' := Spec.insert value tree
+   insert value (toSpec tree) = ↑tree' ∧ well_formed tree'
 := by
-  rw [BSTreeIsize.insert]
   cases tree <;> simp
   case Node curr left right =>
     intro left_wf right_wf left_inv right_inv
-    split_ifs
+    have ⟨left'_spec, left'_wf⟩:= insert_spec left value left_wf
+    have ⟨right'_spec, right'_wf⟩:= insert_spec right value right_wf
+    split_ifs <;> simp [*] at *
     case pos value_lt_curr =>
-      progress as ⟨left', left'_spec, left'_wf⟩; simp
       split_conjs <;> try assumption
       · rw [<-left'_spec]
         simp [Set.insert_union, Set.insert_comm]
       · rw [<-left'_spec]
         simp [value_lt_curr]
         assumption
-    case pos not_value_lt_curr value_gt_curr =>
-      progress as ⟨right', right'_spec, right'_wf⟩
-      simp; split_conjs <;> try assumption
+    case pos curr_lt_value _ =>
+      split_conjs <;> try assumption
       · rw [<-right'_spec]
         simp [Set.insert_union]
       · rw [<-right'_spec]
-        simp [value_gt_curr]
+        simp [curr_lt_value]
         assumption
     case neg not_value_lt_curr not_value_gt_curr =>
       have: curr = value := by scalar_tac
       subst this
       simp; split_conjs <;> assumption
 
+end SetRefinement/- }}} -/
 
 end Lemmas/- }}} -/
-end SetImpl/- }}} -/
