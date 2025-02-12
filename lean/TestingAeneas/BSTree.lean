fe392df37ad18e3e66a16ac2f3b5c5bfe201e528
ayhon <ayhonnick@gmail.com>
2025-02-12 14:12:44 +0100

    refactor: clean the proofs

--- lean/TestingAeneas/BSTree.lean
+++ lean/TestingAeneas/BSTree.lean
@@ -297,8 +297,6 @@ EDIT2: I'm going to try with Mathlib's `Set` again
 end Spec/- }}} -/
 
 namespace Lemmas/- {{{ -/
-@[simp]
-abbrev between[LE α][DecidableLE α](lo hi value: α) := decide (lo <= value ∧ value <= hi)
 
 open testing_aeneas
 section Translation/- {{{ -/
@@ -316,147 +314,94 @@ instance: Coe (BSTree α) (Set α) where
 -- NOTE: This refinement is not injective!
 end Translation/- }}} -/
 
-@[simp] def BSTree.for_all(condition: α -> Bool)(tree: BSTree α) := ∀ x ∈ (tree: Set α), condition x
-
-@[simp] def well_formed[LE α][DecidableLE α](l r: α): BSTree α -> Prop
+@[simp] def well_formed[PartialOrder α][IsTotal α (·<=·)]: BSTree α -> Prop
 | .Nil => True
 | .Node curr left right =>
-  well_formed l curr left ∧ well_formed curr r right ∧ 
-    BSTree.for_all (between l curr) left ∧
-    BSTree.for_all (between curr r) right  ∧
-    between l r curr
-    
+  well_formed left ∧ well_formed right ∧ 
+    (∀ x ∈ (left: Set α),  x < curr) ∧
+    (∀ x ∈ (right: Set α), curr < x)
+
+theorem left_disjoint_right_of_wf[PartialOrder α][IsTotal α (·<=·)]
+  {curr: α}{left right: BSTree α}
+: well_formed (.Node curr left right) -> Disjoint (toSpec left) (toSpec right)
+:= by
+  intro ⟨left_wf, right_wf, left_inv, right_inv⟩
+  apply disjoint_iff.mpr; simp
+  apply Set.subset_empty_iff.mp
+  intro x ⟨xl, xr⟩
+  have h1:=left_inv x xl 
+  have h2:=right_inv x xr
+  have := Trans.trans h1 h2
+  have := lt_irrefl x
+  contradiction
 
 @[pspec]
 theorem contains_spec(tree: BSTree Isize)(target: Isize)
-: well_formed l r tree 
+: well_formed tree 
 -> ∃ b,
     BSTreeIsize.contains tree target = Result.ok b ∧
     (b = true ↔ target ∈ (tree : Set Isize))
 := by 
   intro well_formed
   rw [BSTreeIsize.contains]
-  match tree with
-  | .Nil => simp
-  | .Node curr left right => 
-    simp 
-    simp at well_formed
-    obtain ⟨left_wf, right_wf, left_inv, right_inv, nonempty⟩ := well_formed -- Clears away the `well_formed` hypothesis, which is no longer needed
-    split
-    case isTrue curr_eq_target =>
-      simp [curr_eq_target]
-    case isFalse curr_neq_target =>
-      split
-      simp [Scalar.eq_equiv _ _ |>.not.mp curr_neq_target]
-      case isTrue target_lt_curr => 
-        progress as ⟨left_contains_target, left_contains_target_spec⟩
-        simp [left_contains_target_spec]
-        have: target ∉ (toSpec right) := by
-          intro target_in_right
-          have ⟨lb, ub⟩:= right_inv target target_in_right
-          /- scalar_tac -- target_lt_curr & lb should close this, no? -/
-          have := Trans.trans target_lt_curr lb
-          scalar_tac
-        simp [this]
-      case isFalse target_ge_curr =>
-        have target_ge_curr := (le_of_not_gt target_ge_curr)
-        progress as ⟨right_contains_target, right_contains_target_spec⟩
-        simp [right_contains_target_spec, curr_neq_target]
-        have: target ∉ (toSpec left) := by
-          intro target_in_left
-          have ⟨lb, ub⟩:= left_inv target target_in_left
-          have: curr < (target: Int) := by scalar_tac
-          /- scalar_tac -- Also fails here -/
-          have := Trans.trans ub this
-          scalar_tac
-        simp [this]
-
-theorem nil_of_wellformed_of_gt[BEq α][LawfulBEq α][LE α][DecidableLE α][IsTrans α (·≤·)][BoundedOrder α](tree: BSTree α)
-: ¬ (l <= r) -> well_formed l r tree -> tree = .Nil
-:= by
-  intro l_gt_r
-  intro tree_wf
-  match tree with
-  | .Nil => rfl
-  | .Node .. =>
-    have ⟨left_inv, right_inv, left_wf, right_wf, non_empty⟩ := tree_wf
-    simp at non_empty
-    have ⟨lb, ub⟩ := non_empty
-    have: l <= r := Trans.trans lb ub
-    simp at *; contradiction
+  cases tree <;> simp at *
+  case Node curr left right =>
+    have left_disjoint_right := left_disjoint_right_of_wf well_formed
+    obtain ⟨left_wf, right_wf, left_inv, right_inv⟩ := well_formed
+    split_ifs <;> simp [*]
+    case pos curr_neq_target target_lt_curr =>
+      progress as ⟨left_contains_target, left_contains_target_spec⟩
+      if left_contains_target then
+        simp_all
+      else
+        simp_all
+        intro target_in_right
+        have := right_inv _ target_in_right
+        omega
+    case neg curr_neq_target target_ge_curr =>
+      have target_ge_curr := (le_of_not_gt target_ge_curr)
+      progress as ⟨right_contains_target, right_contains_target_spec⟩
+      if right_contains_target then
+        simp_all
+      else
+        simp_all
+        intro target_in_left
+        have := left_inv _ target_in_left
+        omega
 
 @[pspec]
 theorem insert_spec(tree: BSTree Isize)(value: Isize)
-: well_formed l r tree
+: well_formed tree
 -> ∃ tree',
     BSTreeIsize.insert tree value = Result.ok tree' ∧
     insert value (toSpec tree) = ↑tree' ∧
-    well_formed (minOfLe.min l value) (maxOfLe.max r value) tree'
+    well_formed tree'
 := by
-  intro well_formed
   rw [BSTreeIsize.insert]
-  cases tree
-  case Nil => simp
+  cases tree <;> simp
   case Node curr left right =>
-    simp
-    have ⟨left_wf, right_wf, left_inv, right_inv, nonempty⟩ := well_formed 
-    simp at nonempty left_inv right_inv
-    split
-    case isTrue value_lt_curr =>
-      progress as ⟨left', left'_spec, left'_wf⟩
-      simp; split_conjs
+    intro left_wf right_wf left_inv right_inv
+    split_ifs
+    case pos value_lt_curr =>
+      progress as ⟨left', left'_spec, left'_wf⟩; simp
+      split_conjs <;> try assumption
       · rw [<-left'_spec]
         simp [Set.insert_union, Set.insert_comm]
-      · simp [maxOfLe, le_of_lt value_lt_curr] at left'_wf
-        assumption
-      · have: value < (r : Int) := Trans.trans value_lt_curr nonempty.right
-        simp [minOfLe, le_of_lt this]
-        assumption
       · rw [<-left'_spec]
-        simp [le_of_lt value_lt_curr]
-        intro x x_in_left
-        have⟨_, _⟩ := left_inv x x_in_left
-        split_conjs 
-        · left; assumption
-        · assumption
-      · intro x x_in_right
-        have⟨_, _⟩ := right_inv x x_in_right
-        split_conjs 
-        · assumption
-        · left; assumption
-      · right; apply le_of_lt; assumption
-      · left; exact nonempty.right
-    case isFalse not_value_lt_curr =>
-      split
-      case isTrue value_gt_curr =>
-        progress as ⟨right', right'_spec, right'_wf⟩
-        simp; split_conjs
-        · rw [<-right'_spec]
-          simp [Set.insert_union]
-        · have := Trans.trans nonempty.left value_gt_curr
-          simp [minOfLe, le_of_lt this]
-          assumption
-        · simp [maxOfLe, le_of_lt value_gt_curr] at right'_wf
-          assumption
-        · intro x x_in_left
-          have⟨_, _⟩ := left_inv x x_in_left
-          split_conjs 
-          · left; assumption
-          · assumption
-        · rw [<-right'_spec]
-          simp [le_of_lt value_gt_curr]
-          intro x x_in_right
-          have⟨_, _⟩ := right_inv x x_in_right
-          split_conjs 
-          · assumption
-          · left; assumption
-        · left; exact nonempty.left
-        · left; exact nonempty.right
-      case isFalse not_value_gt_curr =>
-        have: curr = value := by scalar_tac
-        subst this
-        simp [maxOfLe, minOfLe, nonempty]
-        split_conjs <;> assumption
+        simp [value_lt_curr]
+        assumption
+    case pos not_value_lt_curr value_gt_curr =>
+      progress as ⟨right', right'_spec, right'_wf⟩
+      simp; split_conjs <;> try assumption
+      · rw [<-right'_spec]
+        simp [Set.insert_union]
+      · rw [<-right'_spec]
+        simp [value_gt_curr]
+        assumption
+    case neg not_value_lt_curr not_value_gt_curr =>
+      have: curr = value := by scalar_tac
+      subst this
+      simp; split_conjs <;> assumption
 
 
 end Lemmas/- }}} -/
