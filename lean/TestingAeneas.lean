e682c923e70c50b3cdb3dda9c8ecf2d09e2eccb3
ayhon <ayhonnick@gmail.com>
2025-03-17 11:52:22 +0100

    fix: make `lake build` actually check the files

--- lean/TestingAeneas.lean
+++ lean/TestingAeneas.lean
@@ -1,3 +1,5 @@
 -- This module serves as the root of the `TestingAeneas` library.
 -- Import modules here that should be built as part of the library.
-import TestingAeneas.Basic
+import TestingAeneas.BSTree
+import TestingAeneas.AVLTree
+/- import TestingAeneas. -/
